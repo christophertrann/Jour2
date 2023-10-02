7688204ace882024262b4b5de40c31e06dcf9488
christophertrann <christophertran@outlook.fr>
2023-10-02 17:08:27 +0200

    ex_06

---
+++ ex_06.js/ex_06.js
@@ -0,0 +1,10 @@
+let bananas = 1
+if (bananas === 0){
+displayThisText('Oh no there is no banana')
+}
+if (bananas > 0){
+displayThisText('Yummy!')
+}
+if (bananas < 0){
+displayThisText('Do I owe you bananas ?')
+}
