c4eda3d0a3a6dc5d2d4890ab2078946124a66b17
christophertrann <christophertran@outlook.fr>
2023-10-02 17:53:28 +0200

    ex 08

---
+++ ex_08.js/ex_08.js
@@ -0,0 +1,16 @@
+if (breadCount >= 2 )
+if (hamCount >= 1 ){
+displayThisText('I can make a ham sandwich!')
+}
+if (breadCount >= 2 )
+if (tunaCount >= 1 ){
+displayThisText('I can make a tuna sandwich!')
+}
+if (breadCount >= 2 )
+if (hamCount >= 1 )
+if (tunaCount >= 1 ){
+displayThisText('I can make a royal sandwich!')}
+else
+{
+displayThisText('Id rather be fasting tonight..')
+}
