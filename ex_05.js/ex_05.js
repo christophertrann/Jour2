39888fc2afb8e76f34f11069adc7a5d6c41b5fb0
christophertrann <christophertran@outlook.fr>
2023-10-02 16:40:33 +0200

    ex_05

---
+++ ex_05.js/ex_05.js
@@ -0,0 +1,7 @@
+let firstname = 'John'
+let lastname = 'Doe'
+let cardCode = '1111 2222 3333 4444'
+let expiryDateMonth = '21'
+let expiryDateYear = '2042'
+let computesBankInfo = 'John,Doe,1111 2222 3333 4444 5555,21/2042'
+displayInCard(computesBankInfo)
