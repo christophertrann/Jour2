40d52353c1e89042c3178d3129021bd28e6afcff
christophertrann <christophertran@outlook.fr>
2023-10-02 15:00:51 +0200

    ex_02

---
+++ ex_02.js/ex_02.js
@@ -0,0 +1,10 @@
+let integer = "true"
+let float = "forty two"
+let string = "42"
+let bool = "null"
+let this_is_null = "42.42"
+displayThisText(integer)
+displayThisText(float)
+displayThisText(string)
+displayThisText(bool)
+displayThisText(this_is_null)
